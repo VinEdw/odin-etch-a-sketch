a95087add3f00f7812e80755761f0afc58a8398d
Vincent Edwards <mister.man489@gmail.com>
2024-12-15 19:41:03 -0800

    Create and call function to create the grid

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 const gridContainer = document.querySelector("#grid-container");
 const resolutionButton = document.querySelector("#grid-resolution-button");
+
+function createGrid(container, resolution) {
+  container.textContent = "";
+  for (let i = 0; i < resolution; ++i) {
+    const row = document.createElement("div");
+    row.classList.add("grid-row");
+    for (let j = 0; j < resolution; ++j) {
+      const cell = document.createElement("div");
+      cell.classList.add("grid-cell");
+      cell.dataset.opacity = 0;
+      row.appendChild(cell);
+    }
+    container.appendChild(row);
+  }
+}
+createGrid(gridContainer, 16);
